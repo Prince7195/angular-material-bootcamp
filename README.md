7f0ccd4721dc8e74417ec65989b1797d8ee3d46f
Vijay Deepak <vijaydeepak7195@outlook.com>
2018-05-20 23:54:45 +0530

    side-nav

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # AngularMaterialBootcamp
     A Simple Examples of Angular Material Components.
 
+### CMD's:
+    ng add @angular/material  ---  to add angular material to our project
+
+    ng generate @angular/material:material-nav --name=main-nav  ---  Creates an Navbar Angular Component
+
 ### References:
 
 #### Button:
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatToolbarModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { ButtonComponent } from './button/button.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
+    ButtonComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
